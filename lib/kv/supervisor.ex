88e528568ec2c5bf6be01ea0ca32f23248f91744
Joey Rosztoczy <joeyr@stotzeq.com>
2017-05-12 17:37:10 -0700

    sup notes

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -6,6 +6,9 @@ defmodule KV.Supervisor do
   end
 
   def init(:ok) do
+     # After listing child name, we could specific options like restart value
+     # default is permanent (always restarted), others are temporary (never restarted)
+     # and transient (only restarted from abnormal kill msg)
     children = [
       worker(KV.Registry, [KV.Registry]),
       supervisor(KV.Bucket.Supervisor, [])
